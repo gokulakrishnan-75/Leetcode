001db44513334e30ccaff08f0f3fb76b310260f1
GOKULA KRISHNAN D <gokulakrishnan.d75@gmail.com>
2026-09-12 14:05:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (14.04%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,13 @@
+int maxProfit(int* prices, int pricesSize) {
+    int min = prices[0], max = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < min)
+            min = prices[i];
+
+        if (prices[i] - min > max)
+            max = prices[i] - min;
+    }
+
+    return max;
+}
